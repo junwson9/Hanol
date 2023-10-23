5e1b7c254e26e7e2ccf3a4191a83612d83341d85
HBumzz <choi12920@gmail.com>
2023-10-24 00:24:24 +0900

    CHORE: tailwind, font init
    Former-commit-id: f9e3b7db1e8950224961f60777efad8c0804adc6

---
+++ FE/hanol/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        Main: '#3FCC8A',
+        GrayForTab: 'BCBCBC',
+        GrayForText: '888888',
+        White: 'FFFEFF',
+        Black: '252321',
+        Error: 'E30F0F',
+      },
+    },
+  },
+};
