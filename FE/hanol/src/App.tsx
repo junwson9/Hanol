7a59a52769aa2bf3ed6f28c3b9ad697f69adbed9
송준우 <songjunwoo@junsong.local>
2023-10-24 22:57:36 +0900

    FIX: fix git conflict
    Former-commit-id: 90cf9fe5692c40334d9cab0bec954b617c711fe7

--- FE/hanol/src/App.tsx
+++ FE/hanol/src/App.tsx
@@ -1,18 +1,3 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ValueCard from './components/DashboardPage/ValueCard';
-import ValueGraph from './components/DashboardPage/ValueGraph';
-import './App.css';
-
-function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<ValueCard title="탈모" value={2} />} />
-				<Route path="/graph" element={<ValueGraph />} />
-			</Routes>
-		</BrowserRouter>
-	);
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/auth/login';
 import LoginError from './pages/auth/loginError';
