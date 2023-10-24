4c4462e8161685e2fd29b80583b96d564e1f7e2d
HBumzz <choi12920@gmail.com>
2023-10-24 23:35:46 +0900

    FEAT: add components, login publishing implement
    Former-commit-id: e686d22e05f401c07db714b8b1cbea06bce95269

--- FE/hanol/src/pages/auth/signupBirth.tsx
+++ FE/hanol/src/pages/auth/signupBirth.tsx
@@ -3,6 +3,7 @@ import 'react-datepicker/dist/react-datepicker.css';
 import DateValidationDisableFuture from 'components/picker/DatePicker';
 import Button from 'components/button/Button';
 import { useNavigate } from 'react-router-dom';
+import TapBarDepth2 from 'components/common/TapBarDepth2';
 
 function SignupBirth() {
   const buttonName = '확인';
@@ -12,11 +13,22 @@ function SignupBirth() {
     navigate('/signup-gender');
   };
 
+  const handleCloseClick = () => {
+    navigate('/login');
+  };
+
   return (
     <div className="col-span-full h-screen flex flex-col justify-between">
       <div>
-        <div className="mt-[105px] font-bold text-[18px] text-black flex">가입을 축하드려요!</div>
-        <div className=" font-bold text-[18px] text-black flex">생년월일을 알려주세요.</div>
+        <TapBarDepth2 name={'회원가입'} onClick={handleCloseClick} />
+        <div className="flex">
+          <div className="h-[0.188rem] w-[33%] bg-Main absolute left-0"></div>
+          <div className="h-[0.188rem] w-[67%] bg-Gray absolute left-[33%]"></div>
+        </div>
+      </div>
+      <div>
+        <div className="font-bold text-[18px] text-black flex">가입을 축하드려요!</div>
+        <div className="font-bold text-[18px] text-black flex">생년월일을 알려주세요.</div>
         <div className="mt-[12px] font-regular text-[12px] text-GrayForText flex">
           나이에 따라 분석 결과가 달라집니다.
         </div>
@@ -24,6 +36,7 @@ function SignupBirth() {
       <div>
         <DateValidationDisableFuture />
       </div>
+      <div></div>
       <div className="mb-[4rem]">
         <Button name={buttonName} onClick={handleButtonClick} />
       </div>
