308df86a4b6d4079448a0a632a4167c8e21cb317
HBumzz <choi12920@naver.com>
2023-10-24 11:07:51 +0900

    FEAT: gird, button component chore
    Former-commit-id: e0d3402297701f8152f85da933be48df45c7b408

--- FE/hanol/src/pages/auth/login.tsx
+++ FE/hanol/src/pages/auth/login.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import FloatingButton from 'components/button/FloatingButton';
+import Button from 'components/button/Button';
+import EmptyButton from 'components/button/EmptyButton';
 
 function Login() {
   const login = 'login';
   return (
-    <div>
-      <p>{login}</p>
+    <div className="col-span-full">
+      <p className="font-medium">{login}</p>
       <div>
         <FloatingButton />
+      </div>
+      <div>
+        <Button />
+      </div>
+      <div>
+        <EmptyButton />
       </div>
     </div>
   );
