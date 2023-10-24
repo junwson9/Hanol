10beef8834c29da16a0c751222e46481bec99951
최홍준 <choi12920@naver.com>
2023-10-24 13:31:50 +0900

    FEAT<Client>: loginError page implement
    Former-commit-id: f61cebfc9350278d6338b9e7316a59e6fee5879d

--- FE/hanol/src/components/button/Button.tsx
+++ FE/hanol/src/components/button/Button.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-function Button() {
-  const name = 'name';
+type ButtonProps = {
+  name: string;
+  onClick: () => void;
+};
+
+function Button({ name, onClick }: ButtonProps) {
   return (
     <div className="w-full h-[54px] rounded-xl justify-center items-center gap-2.5 inline-flex bg-Main">
-      <div className="text-white text-base font-medium text-[16px] tracking-wide">{name}</div>
+      <div onClick={onClick} className="text-white text-base font-medium text-[16px] tracking-wide">
+        {name}
+      </div>
     </div>
   );
 }
