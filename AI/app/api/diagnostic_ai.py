1925fcd7736a129a07a08d3765cc8cae4be319ca
김준현 <idkim8671@gmail.com>
2023-10-25 00:57:31 +0900

    FIX: model-server fix
    Former-commit-id: bf8cbc655ad48c815c21f3d2ff21835bb7b23cf8

--- AI/app/api/diagnostic_ai.py
+++ AI/app/api/diagnostic_ai.py
@@ -1,19 +1,20 @@
 import torch
+import time
 from torchvision import transforms
 from PIL import Image
-import time
+from io import BytesIO
+from fastapi import UploadFile
 
 class DiagnosticAI:
-    def __init__(self) -> None:
-        pass
+    async def process_diagnostic(self, file: UploadFile):
+        image_bytes = await file.read()
 
-    async def process_diagnostic(self, request: Image):
+        image = Image.open(BytesIO(image_bytes))
         time1 = time.time()
-        image = Image
         model_path = 'AI_model/test_model1.pt'
         model = torch.load(model_path, map_location=torch.device('cpu'))
 
-        image_size = (600, 600)  # 모델의 입력 이미지 크기에 맞게 설정
+        image_size = (600, 600)
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         preprocess = transforms.Compose([
             transforms.Resize(image_size),
@@ -29,15 +30,13 @@ class DiagnosticAI:
 
         probabilities = torch.nn.functional.softmax(output[0], dim=0)
 
-        # 클래스별 확률과 예측 클래스를 출력합니다.
-        class_names = ['클래스0', '클래스1', '클래스2', '클래스3']  # 클래스 이름을 모델에 따라 수정하세요.
+        class_names = ['클래스0', '클래스1', '클래스2', '클래스3']
         top_p, top_class = probabilities.topk(1, dim=0)
         predicted_class = class_names[top_class]
 
         print(f'예측 클래스: {predicted_class}, 확률: {top_p.item()}')
         time2 = time.time()
 
-        print(time2-time1)
+        print(time2 - time1)
 
-        return {"예측 클래스": predicted_class,"확률":top_p.item()}
-    
+        return {"예측 클래스": predicted_class, "확률": top_p.item()}
