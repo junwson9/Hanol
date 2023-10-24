f373134ef5b005e0ce9e6229b786211119e3f78b
nahye2772 <nahye2772@naver.com>
2023-10-24 14:03:08 +0900

    FEAT: add ValueCard Component
    Former-commit-id: e97c342b39277d35fdcc9bf34074fe13ef955d0c

--- FE/hanol/src/App.tsx
+++ FE/hanol/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import TopTab from './components/common/TopTab';
+import ValueCard from './components/DashboardPage/ValueCard';
 import './App.css';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<TopTab Indicator="one" />} />
+				<Route path="/" element={<ValueCard title="탈모" value={2} />} />
 			</Routes>
 		</BrowserRouter>
 	);
