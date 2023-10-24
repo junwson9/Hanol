d4889fa66d349c6329dd7f796474f0fbcfc1c597
김준현 <idkim8671@gmail.com>
2023-10-24 17:35:31 +0900

    FEAT[AI]: Implement AI-server
    Former-commit-id: 38b2a26aeaed9c2b6b65b0aad2d1b91837d3a6f7

--- AI/app/main.py
+++ AI/app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from PIL import Image
+import api
 
 app = FastAPI()
 
 @app.get("/test")
 async def test():
     return 'test page'
+
+@app.post("/image")
+async def process_diagnostic(request:Image):
+    return await api.diagnostic_ai(request)
