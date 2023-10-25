c367dae86d9a4c7ea413a460509fdf87d4cc2919
nahye2772 <nahye2772@naver.com>
2023-10-25 14:31:45 +0900

    FEAT: add BannerButton component
    Former-commit-id: f691da420e02cdb39fec2b6f49bb430ff0b5600e

--- FE/hanol/src/App.tsx
+++ FE/hanol/src/App.tsx
@@ -15,7 +15,7 @@ import React from 'react';
 // 	);
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/auth/login';
-import RecommendCareRoutine from './components/DashboardPage/RecommendCareRoutine';
+import BannerButton from './components/button/BannerButton';
 import './App.css';
 
 function App() {
@@ -25,7 +25,7 @@ function App() {
         <Route path="/login" element={<Login />} />
       </Routes>
       <Routes>
-        <Route path="/test" element={<RecommendCareRoutine />} />
+        <Route path="/test" element={<BannerButton />} />
       </Routes>
     </div>
   );
