7a577918ed6f7cecfc0672efdb16e64eb6a2f7ed
김지수 <spritaa@gmail.com>
2023-10-26 10:45:57 +0900

    FEAT<Server> implement getting suggested routines query
    Former-commit-id: 5432d818cb749369b659cd0031d3e1f2d8974534

--- BE/hanol/src/main/java/com/ssafy/hanol/routine/service/RoutineService.java
+++ BE/hanol/src/main/java/com/ssafy/hanol/routine/service/RoutineService.java
@@ -1,8 +1,8 @@
 package com.ssafy.hanol.routine.service;
 
-import com.ssafy.hanol.member.domain.Member;
+import com.ssafy.hanol.diagnosis.domain.Diagnosis;
+import com.ssafy.hanol.diagnosis.repository.DiagnosisRepository;
 import com.ssafy.hanol.routine.domain.MemberRoutine;
-import com.ssafy.hanol.routine.domain.MemberRoutineLog;
 import com.ssafy.hanol.routine.domain.Routine;
 import com.ssafy.hanol.routine.repository.MemberRoutineLogRepository;
 import com.ssafy.hanol.routine.repository.MemberRoutineRepository;
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,38 +28,31 @@ public class RoutineService {
     private final RoutineRepository routineRepository;
     private final MemberRoutineRepository memberRoutineRepository;
     private final MemberRoutineLogRepository memberRoutineLogRepository;
+    private final DiagnosisRepository diagnosisRepository;
 
     public RoutineListResponse findRoutineList() {
         // 임시 데이터
         Long memberId = 1L;
-        List<RoutineListResponse.RoutineItem> suggestedRoutines = new ArrayList<>();
-        for(Long i=1L; i<=30L; i=i+5) {
-            suggestedRoutines.add(RoutineListResponse.RoutineItem.builder().memberRoutineId(null).routineId(i).routineName("더미데이터"+i).build());
-        }
 
         // 회원이 설정해둔 루틴 리스트 조회
         List<MemberRoutine> memberRoutines = memberRoutineRepository.findByMemberId(memberId);
 
-        List<RoutineListResponse.RoutineItem> myRoutines = memberRoutines.stream()
-                .map(RoutineListResponse.RoutineItem::from)
+        List<RoutineInfo> myRoutines = memberRoutines.stream()
+                .map(RoutineInfo::from)
                 .collect(Collectors.toList());
 
-//        // TODO Diagnosis 엔티티, 레포지토리 개발 후 구현 가능
-//        // 최신 진단 결과 조회
-//        Diagnosis latestDiagnosis = diagnosisRepository.findTopByMemberIdByIdDesc(memberId).orElse(null);
-//
-//        // 진단 결과가 존재하면 value 값들을 리스트로 받기
-//        List<Integer> values = new ArrayList<>();
-//        if(latestDiagnosis !=  null) {
-//            values = latestDiagnosis.getValuesAsList();
-//        }
-//
-//        // QueryDSL 이용
-//        // is_default가 true이거나,
-//        // values가 !isEmpty() 이고 회원의 value값이 1이상인 경우 is_value값이 true이면서
-//        // memberRoutines과 중복되지 않는 루틴 선택
-//        List<RoutineListResponse.RoutineItem> suggestedRoutines = routineRepository.findByValuesAndNotMemberRoutines(memberId, values);
+        // 최신 진단 결과 조회
+        Diagnosis latestDiagnosis = diagnosisRepository.findTopByMemberIdOrderByIdDesc(memberId).orElse(null);
 
+        // 진단 결과가 존재하면 value 값들을 리스트로 받기
+        List<Integer> values = new ArrayList<>();
+        if(latestDiagnosis !=  null) {
+            values = latestDiagnosis.getValuesAsList();
+        }
+
+        // 추천 루틴 조회 : valueX값이 1이상이거나 isDefault=true인 루틴 중, memberRoutines에 없는 루틴.
+        List<RoutineInfo> suggestedRoutines = routineRepository.findByValuesAndNotMemberRoutines(memberId, values, memberRoutines);
+        log.info("추천 루틴 suggestedRoutines: {}", suggestedRoutines);
 
         return RoutineListResponse.builder()
                 .myRoutines(myRoutines)
