05a354c8d076852784fae697df05aa281a7c18bb
송준우 <boaz42@khu.ac.kr>
2023-10-26 17:48:27 +0900

    FEAT<Client>: explain diagnose publishing
    Former-commit-id: c37ddae7d7f22ad84304f4b9b5739f43e3d705be

--- FE/hanol/src/components/common/TopTab.tsx
+++ FE/hanol/src/components/common/TopTab.tsx
@@ -3,89 +3,87 @@ import React from 'react';
 import styled from 'styled-components';
 
 interface Props {
-	Indicator: string;
+  Indicator: string;
 }
 
 const TopTab = ({ Indicator }: Props) => {
-	// const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-	return (
-		<TopTabBox>
-			<LeftBox Indicator={Indicator}>
-				<div className="top_tab_title">간편 문진</div>
+  return (
+    <TopTabBox>
+      <LeftBox Indicator={Indicator}>
+        <div className="top_tab_title">간편 문진</div>
 
-				<UnderBar>{Indicator === 'one' && <ActiveBar />}</UnderBar>
-			</LeftBox>
+        <UnderBar>{Indicator === 'one' && <ActiveBar />}</UnderBar>
+      </LeftBox>
 
-			<RightBox Indicator={Indicator}>
-				<div className="top_tab_title">문진 결과 </div>
-				<UnderBar>{Indicator === 'two' && <ActiveBar />} </UnderBar>
-			</RightBox>
-		</TopTabBox>
-	);
+      <RightBox Indicator={Indicator}>
+        <div className="top_tab_title">문진 결과 </div>
+        <UnderBar>{Indicator === 'two' && <ActiveBar />} </UnderBar>
+      </RightBox>
+    </TopTabBox>
+  );
 };
 
 const ActiveBar = styled.div`
-	width: 142px;
-	height: 2px;
-	flex-shrink: 0;
+  width: 142px;
+  height: 2px;
+  flex-shrink: 0;
 
-	border-radius: 1px;
-	background: #353d4a;
+  border-radius: 1px;
+  background: #353d4a;
 `;
 const UnderBar = styled.div`
-	display: flex;
-	justify-content: center;
+  display: flex;
+  justify-content: center;
 
-	width: 50%;
-	height: 1px;
-	background: #d1d5db;
+  width: 50%;
+  height: 1px;
+  background: #d1d5db;
 `;
 
 const RightBox = styled.div<Props>`
-	display: flex;
-	width: 50%;
-	padding-top: 12px;
-	flex-direction: column;
-	justify-content: flex-end;
-	align-items: center;
-	gap: 13px;
-	cursor: pointer;
-	.top_tab_title {
-		color: ${(props) =>
-			props.Indicator === 'two' ? '#353d4a !important' : '#6d7582'};
-	}
+  display: flex;
+  width: 50%;
+  padding-top: 12px;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 13px;
+  cursor: pointer;
+  .top_tab_title {
+    color: ${(props) => (props.Indicator === 'two' ? '#353d4a !important' : '#6d7582')};
+  }
 `;
 const LeftBox = styled.div<Props>`
-	display: flex;
-	width: 50%;
-	padding-top: 12px;
-	flex-direction: column;
-	justify-content: flex-end;
-	align-items: center;
-	gap: 13px;
-	cursor: pointer;
-	.top_tab_title {
-		color: ${(props) =>
-			props.Indicator === 'one' ? '#353d4a !important' : '#6d7582'};
-	}
+  display: flex;
+  width: 50%;
+  padding-top: 12px;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 13px;
+  cursor: pointer;
+  .top_tab_title {
+    color: ${(props) => (props.Indicator === 'one' ? '#353d4a !important' : '#6d7582')};
+  }
 `;
 const TopTabBox = styled.div`
-	display: inline-flex;
-	align-items: flex-start;
-	width: 100%;
-	background: #fffeff;
+  display: inline-flex;
+  align-items: flex-start;
+  width: 100%;
+  background: #fffeff;
 
-	.top_tab_title {
-		color: #6d7582;
-		text-align: center;
-		font-family: Noto Sans KR;
-		font-size: 16px;
-		font-style: normal;
-		font-weight: 500;
-		line-height: 140%;
-		letter-spacing: 0.1px;
-	}
+  .top_tab_title {
+    color: #6d7582;
+    text-align: center;
+    font-family: Noto Sans KR;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 140%;
+    letter-spacing: 0.1px;
+  }
 `;
 
 export default TopTab;
