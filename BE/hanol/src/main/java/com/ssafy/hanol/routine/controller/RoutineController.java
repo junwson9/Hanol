7567ca07c970f11a76dcae690715fef217f1f6c2
Jisoo <spritaa@gmail.com>
2023-10-27 10:44:17 +0900

    RENAME: rename routines api
    Former-commit-id: 39cd8df518b746b03d3d90ca08a55d6c075b80f7

--- BE/hanol/src/main/java/com/ssafy/hanol/routine/controller/RoutineController.java
+++ BE/hanol/src/main/java/com/ssafy/hanol/routine/controller/RoutineController.java
@@ -40,20 +40,20 @@ public class RoutineController {
         return ResponseFactory.success("루틴 리스트 변경 성공");
     }
 
-    @GetMapping("/daily-routines")
-    public ResponseEntity<?> dailyRoutineLogList(@RequestParam(value = "date") String date) {
+    @GetMapping("/daily-routine")
+    public ResponseEntity<?> dailyRoutineLogList(@RequestParam(value = "date", required = true) String date) {
         RoutineLogListResponse result = routineService.findMemberRoutineLogByDate(LocalDate.parse(date));
         return ResponseFactory.success("특정일의 루틴 이력 리스트 조회 성공", RoutineLogListApiResponse.from(result));
     }
 
-    @GetMapping("/achievement_rates")
-    public ResponseEntity<?> routineAchievementRates(@RequestParam(value = "start-date") String startDate,
-                                                     @RequestParam(value = "end-date") String endDate) {
+    @GetMapping("/daily-routine/achievement-rates")
+    public ResponseEntity<?> routineAchievementRates(@RequestParam(value = "start-date", required = true) String startDate,
+                                                     @RequestParam(value = "end-date", required = true) String endDate) {
         RoutineAchievementRatesResponse result = routineService.findRoutineAchievementRates(LocalDate.parse(startDate), LocalDate.parse(endDate));
         return ResponseFactory.success("기간 내 일별 루틴 달성률 조회 성공", RoutineAchievementRatesApiResponse.from(result));
     }
 
-    @PatchMapping("/{memberRoutineLogId}/achievement")
+    @PatchMapping("/daily-routine/{memberRoutineLogId}/achievement")
     public ResponseEntity<?> routineAchievementStatusModify(@PathVariable Long memberRoutineLogId,
                                                             @Validated @RequestBody RoutineAchievementStatusApiRequest request) {
         RoutineAchievementStatusResponse result = routineService.modifyRoutineAchievementStatus(memberRoutineLogId, request.toApplicationDto());
