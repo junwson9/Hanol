542a5d882311bb6e34a7f0db7f2e863de2cd619a
박승휘 <nahye2772@naver.com>
2023-10-27 09:14:06 +0900

    Feat<Client>: add Myreport-Detail components and page
    Former-commit-id: ff3f9186dc3f310803f99c57864ec854753c70b1

---
+++ FE/hanol/src/components/button/DateNavigateButton.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as LeftNavigateIcon } from '../../assets/icons/chevron_right_FILL0_wght400_GRAD0_opsz24 2.svg';
+import { ReactComponent as RightNavigateIcon } from '../../assets/icons/chevron_right_FILL0_wght400_GRAD0_opsz24 1.svg';
+
+interface Props {
+  date: string;
+}
+
+const DateNavigateButton = ({ date }: Props) => {
+  return (
+    <div className="col-span-full">
+      <ButtonBox>
+        <DateNavigateButtonBox>
+          <LeftNavigateIcon className="left_icon" />
+          <div className="date_box">{date}</div>
+          <RightNavigateIcon className="right_icon" />
+        </DateNavigateButtonBox>
+      </ButtonBox>
+    </div>
+  );
+};
+
+const ButtonBox = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const DateNavigateButtonBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* width: 14.125rem; */
+  width: 19.625rem;
+  height: 2.188rem;
+  border-radius: 18px;
+  border: 0.2px solid var(--GrayForTab, #bcbcbc);
+  opacity: 0.76;
+  background: var(--white, #fffeff);
+
+  .left_icon {
+    margin-left: 0.625rem;
+    cursor: pointer;
+  }
+  .right_icon {
+    margin-right: 0.625rem;
+    cursor: pointer;
+  }
+  .date_box {
+    /* display: flex;
+    flex-direction: column;
+    justify-content: center; */
+
+    width: 12.813rem;
+    height: 0.875rem;
+    flex-shrink: 0;
+
+    color: var(--Black, #252321);
+    text-align: center;
+
+    font-family: Noto Sans KR;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 14px;
+    letter-spacing: 0.4px;
+    cursor: pointer;
+  }
+`;
+
+export default DateNavigateButton;
