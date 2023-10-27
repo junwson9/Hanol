b9d6ba17fb68281e69170841de50c40c1deccb5e
박승휘 <nahye2772@naver.com>
2023-10-27 17:28:55 +0900

    Feat<Client>: add diagnosis-analyzing page, components of detail page
    Former-commit-id: 17932081aaa7c62182fc90ea784b8dc7ab7d6b33

---
+++ FE/hanol/src/components/diagnosisResultPage/RecommendCareRoutine2.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components';
+import illust from '../../assets/images/image35.jpg';
+
+const RecommendCareRoutine2 = () => {
+  return (
+    <div className="col-span-full">
+      <RecMentBox>
+        <div className="title">두피 관리 미루지 마세요</div>
+        <ContentBox>
+          <div className="content">회원님께 꼭 맞는 두피 케어 루틴을 추천해드려요.</div>
+          <div className="content">매일 루틴을 실천하고 두피 건강을 챙기세요.</div>
+        </ContentBox>
+        <IllustrationBox>
+          <img src={illust} alt="3d 일러스트 아이콘" className="illustration" />
+        </IllustrationBox>
+      </RecMentBox>
+    </div>
+  );
+};
+
+const IllustrationBox = styled.div`
+  margin-top: 1.438rem;
+  margin-bottom: 2.876rem;
+  align-self: center;
+`;
+const ContentBox = styled.div`
+  color: var(--GrayForText, #888);
+
+  font-family: Noto Sans KR;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 17px;
+  letter-spacing: 0.4px;
+  margin-top: 1.438rem;
+  text-align: start;
+`;
+const RecMentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  .title {
+    color: var(--Black, #252321);
+    font-family: Noto Sans KR;
+    font-size: 18px;
+    font-weight: 700;
+    line-height: 18px;
+    letter-spacing: 0.2px;
+    text-align: start;
+
+    margin-top: 4.625rem;
+  }
+`;
+export default RecommendCareRoutine2;
