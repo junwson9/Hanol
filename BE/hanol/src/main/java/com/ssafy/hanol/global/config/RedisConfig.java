c90c8b68d8c692bb870310e76feade442b4c4a34
김지수 <spritaa@gmail.com>
2023-10-29 21:48:30 +0900

    FEAT<Server>: implement daily routine scheduling
    Former-commit-id: f0869eb926bdf90c7aa272b11db04ccdc6bf61f3

--- BE/hanol/src/main/java/com/ssafy/hanol/global/config/RedisConfig.java
+++ BE/hanol/src/main/java/com/ssafy/hanol/global/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.ssafy.hanol.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -10,6 +11,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@Configuration
 public class RedisConfig {
     @Value("${spring.redis.host}")
     private String redisHost;
