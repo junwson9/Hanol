9e99664ac5b81b6a76724f7e514ec2aec625c383
Jisoo <spritaa@gmail.com>
2023-10-29 18:45:36 +0900

    FEAT: implement daily routine log scheduling
    Former-commit-id: c0129bc6761de57a078c1293b76a31cfc11fad4d

--- BE/hanol/src/main/java/com/ssafy/hanol/routine/service/RoutineSchedulerService.java
+++ BE/hanol/src/main/java/com/ssafy/hanol/routine/service/RoutineSchedulerService.java
@@ -1,15 +1,16 @@
 package com.ssafy.hanol.routine.service;
 
+import com.ssafy.hanol.common.util.SchedulingStatusUtil;
 import com.ssafy.hanol.routine.domain.MemberRoutine;
 import com.ssafy.hanol.routine.domain.MemberRoutineLog;
 import com.ssafy.hanol.routine.repository.MemberRoutineLogRepository;
 import com.ssafy.hanol.routine.repository.MemberRoutineRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,28 +19,26 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 public class RoutineSchedulerService {
-
-    private final RedisTemplate<String, Object> redisTemplate;
+    private final SchedulingStatusUtil schedulingStatusUtil;
     private final MemberRoutineLogRepository memberRoutineLogRepository;
     private final MemberRoutineRepository memberRoutineRepository;
 
-    private static final String SCHEDULING_TEMPLATE = "%s_status";
-
     @Scheduled(cron = "0 0 0 * * ?") // 매일 자정 수행
+    @Transactional
     public void registerDailyRoutine() {
+        LocalDate today = LocalDate.now();
         String schedulingName = "register_daily_routine";
+        log.info("루틴 입력 스케쥴링 시작: {}", today);
 
         try {
             // Redis에 스케쥴링 상태 업데이트
-            startScheduling(schedulingName);
+            schedulingStatusUtil.startScheduling(schedulingName);
 
-            // memberRoutine 테이블을 조회하면서 memberRoutineLog 테이블을 데이터를 Bulk insert 한다
-            LocalDate today = LocalDate.now();
-
+            // memberRoutine 테이블을 조회하여 memberRoutineLog 테이블에 데이터를 Bulk Insert 한다
             List<MemberRoutine> memberRoutines = memberRoutineRepository.findAll();
 
             List<MemberRoutineLog> routineLogs = new ArrayList<>();
-            for(MemberRoutine routine : memberRoutines) {
+            for (MemberRoutine routine : memberRoutines) {
                 MemberRoutineLog routineLog = MemberRoutineLog.builder()
                         .member(routine.getMember())
                         .routine(routine.getRoutine())
@@ -50,20 +49,13 @@ public class RoutineSchedulerService {
             }
             memberRoutineLogRepository.saveAll(routineLogs);
 
+        } catch (Exception e) {
+            log.error("루틴 입력 스케쥴링 중 에러 발생: {}", e.getMessage(), e);
+
         } finally {
             // Redis에 스케쥴링 상태 업데이트
-            finishedScheduling(schedulingName);
+            schedulingStatusUtil.finishedScheduling(schedulingName);
+            log.info("루틴 입력 스케쥴링 종료: {}", today);
         }
-        
-    }
-
-    public void startScheduling(String schedulingName) {
-        String key = String.format(SCHEDULING_TEMPLATE, schedulingName);
-        redisTemplate.opsForValue().set(schedulingName, "STARTED");
-    }
-
-    public void finishedScheduling(String schedulingName) {
-        String key = String.format(SCHEDULING_TEMPLATE, schedulingName);
-        redisTemplate.opsForValue().set(schedulingName, "FINISHED");
     }
 }
