d61b7f218d735145c8d521e2aa8a475257c32110
송준우 <boaz42@khu.ac.kr>
2023-10-30 14:34:40 +0900

    FEAT: add socket connection
    Former-commit-id: a03acb7ef85dc2bfffeb58f63c75b05aaa50ccc1

--- FE/hanol/src/pages/diagnose/IoTstreaming.tsx
+++ FE/hanol/src/pages/diagnose/IoTstreaming.tsx
@@ -1,4 +1,29 @@
+import { useRef, useEffect } from 'react';
 function IoTstreaming() {
-  return <div>여기에 IoT 연결화면이 들어갑니다.</div>;
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    const socket = new WebSocket('ws://raspberrypi_ip:8765'); // Replace with your Raspberry Pi's IP address
+
+    socket.onmessage = (event) => {
+      const frame_base64 = event.data;
+      const frame = 'data:image/jpeg;base64,' + frame_base64;
+
+      if (videoRef.current) {
+        videoRef.current.src = frame;
+      }
+    };
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  return (
+    <div>
+      <video ref={videoRef} autoPlay controls />
+    </div>
+  );
 }
+
 export default IoTstreaming;
