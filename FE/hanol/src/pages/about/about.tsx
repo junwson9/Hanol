85632f1616462d0a3494ac48883659a90f2a6564
최홍준 <choi12920@naver.com>
2023-10-30 12:27:05 +0900

    FEAT<Client>: Implement interceptor, mypage publishing
    Former-commit-id: 0e53454855a315f8e48f67b4c2dadcf376168ec3

--- FE/hanol/src/pages/about/about.tsx
+++ FE/hanol/src/pages/about/about.tsx
@@ -1,6 +1,11 @@
 import TapBar from 'components/common/TopBar';
+import { useNavigate } from 'react-router-dom';
 
 function About() {
+  const navigate = useNavigate();
+  const handleButtonClick = () => {
+    navigate('/login');
+  };
   return (
     <div className="col-span-full">
       <div>
@@ -8,7 +13,10 @@ function About() {
       </div>
       <div className="h-[7rem] flex items-center justify-between border-y mb-[1rem]">
         <div className="text-[#999999] font-regular ">로그인이 필요합니다.</div>
-        <button className="bg-[#F4F4F4] px-[0.5rem] py-[0.2rem] rounded-[0.3rem] text-[#555555] font-regular text-[0.813rem]">
+        <button
+          onClick={handleButtonClick}
+          className="bg-[#F4F4F4] px-[0.5rem] py-[0.2rem] rounded-[0.3rem] text-[#555555] font-regular text-[0.813rem]"
+        >
           로그인
         </button>
       </div>
