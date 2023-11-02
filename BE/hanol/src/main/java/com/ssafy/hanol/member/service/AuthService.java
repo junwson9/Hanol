411c5177bc844e5d1028c69dadd91dd0c4620eb7
jlal1226 <jlal1226@naver.com>
2023-11-02 17:46:42 +0900

    FEAT: implement member sign up, reissue, logout, info
    Former-commit-id: 0ef7b1378fe2a2b4f4a74adef8711b181697ebab

--- BE/hanol/src/main/java/com/ssafy/hanol/member/service/AuthService.java
+++ BE/hanol/src/main/java/com/ssafy/hanol/member/service/AuthService.java
@@ -1,10 +1,16 @@
 package com.ssafy.hanol.member.service;
 
+import com.ssafy.hanol.common.exception.CustomException;
 import com.ssafy.hanol.member.domain.Member;
+import com.ssafy.hanol.member.domain.Role;
 import com.ssafy.hanol.member.domain.Token;
+import com.ssafy.hanol.member.exception.AuthenticationErrorCode;
+import com.ssafy.hanol.member.exception.MemberErrorCode;
 import com.ssafy.hanol.member.repository.MemberRepository;
 import com.ssafy.hanol.member.service.dto.OauthLoginRequest;
 import com.ssafy.hanol.member.service.dto.OauthLoginResponse;
+import com.ssafy.hanol.member.service.dto.TokenReissueRequest;
+import com.ssafy.hanol.member.service.dto.TokenReissueResponse;
 import com.ssafy.hanol.member.service.oidc.IdTokenValidator;
 import com.ssafy.hanol.member.service.token.TokenService;
 import lombok.RequiredArgsConstructor;
@@ -29,17 +35,43 @@ public class AuthService {
                         oauthMemberInfo.getOauthProvider())
                 .orElse(null);
 
+
         if (isRequireSignUp(oauthMember)) {
             oauthMember = memberRepository.save(oauthMemberInfo.toMember());
         }
+        oauthMember.updateLastLoginDate();
 
         Token accessToken = tokenService.createAccessToken(oauthMember);
         Token refreshToken = tokenService.createRefreshToken();
         tokenService.saveRefreshToken(refreshToken, oauthMember.getId());
-        return new OauthLoginResponse(accessToken, refreshToken);
+
+        return new OauthLoginResponse(accessToken, refreshToken, Role.GUEST.toString());
+    }
+
+    public TokenReissueResponse reissue(TokenReissueRequest request){
+        Member member = findMemberByMemberId(request.getMemberId());
+        Token token = tokenService.getRefreshTokenFromMember(member)
+                .orElseThrow(() -> new CustomException(
+                        AuthenticationErrorCode.REQUIRE_LOGIN));
+        if(!isCorrespondToken(request.getRefreshToken(), token)){
+            throw new CustomException(AuthenticationErrorCode.UN_AUTHORIZATION);
+        }
+        return new TokenReissueResponse(tokenService.createAccessToken(member));
+    }
+
+    public void logout(Long memberId){
+        tokenService.deleteRefreshToken(memberId);
     }
 
     private boolean isRequireSignUp(Member oauthMember) {
         return oauthMember == null;
+    }
+
+    private boolean isCorrespondToken(Token inputToken, Token savedToken){
+        return inputToken.getToken().equals(savedToken.getToken());
+    }
+
+    private Member findMemberByMemberId(Long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(() -> new CustomException(MemberErrorCode.NOT_FOUND_MEMBER));
     }
 }
