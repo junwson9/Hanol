10c6f3180ef3d86f9a334427ae8eda280ff48a13
nahye2772 <nahye2772@naver.com>
2023-11-03 17:55:24 +0900

    FEAT: add fcm api
    Former-commit-id: 269e9cf6a1e6ac11a8ddad6df56718721079bacb

--- FE/hanol/src/firebase-messaging-sw.js
+++ FE/hanol/src/firebase-messaging-sw.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
-import { getMessaging, getToken, onMessage } from 'firebase/messaging';
-import axiosInstance from 'api/axiosInterceptor';
+// import { getMessaging, getToken, onMessage } from 'firebase/messaging';
+import { getMessaging, onMessage } from 'firebase/messaging';
+// import axiosInstance from 'api/axiosInterceptor';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -30,12 +31,12 @@ async function requestPermission() {
 
   console.log('알림 권한이 허용됨');
 
-  const token = await getToken(messaging, {
-    vapidKey: process.env.REACT_APP_VAPID_KEY,
-  });
+  // const token = await getToken(messaging, {
+  //   vapidKey: process.env.REACT_APP_VAPID_KEY,
+  // });
 
-  if (token) console.log('token: ', token);
-  else console.log('Can not get Token');
+  // if (token) console.log('token: ', token);
+  // else console.log('Can not get Token');
 
   onMessage(messaging, (payload) => {
     console.log('메시지가 도착했습니다.', payload);
@@ -45,23 +46,23 @@ async function requestPermission() {
 
 requestPermission();
 
-async function sendTokenToServer() {
-  const token = await getToken(messaging, {
-    vapidKey: process.env.REACT_APP_VAPID_KEY,
-  });
+// async function sendTokenToServer() {
+//   const token = await getToken(messaging, {
+//     vapidKey: process.env.REACT_APP_VAPID_KEY,
+//   });
 
-  // 사용자가 로그인한 후, 해당 토큰을 서버로 전송하고 서버에서 필요한 처리를 진행합니다.
-  if (token) {
-    // 토큰을 서버로 전송하기 위해 Axios나 fetch 등을 사용할 수 있습니다.
-    try {
-      const response = await axiosInstance.post('/notifications/token', { fcm_token: token });
-      console.log('토큰을 서버로 전송했습니다.', response.data);
-    } catch (error) {
-      console.error('토큰을 서버로 전송하는 중 에러 발생:', error);
-    }
-  } else {
-    console.log('토큰을 가져오지 못했습니다.');
-  }
-}
+//   // 사용자가 로그인한 후, 해당 토큰을 서버로 전송하고 서버에서 필요한 처리를 진행합니다.
+//   if (token) {
+//     // 토큰을 서버로 전송하기 위해 Axios나 fetch 등을 사용할 수 있습니다.
+//     try {
+//       const response = await axiosInstance.post('/notifications/token', { fcm_token: token });
+//       console.log('토큰을 서버로 전송했습니다.', response.data);
+//     } catch (error) {
+//       console.error('토큰을 서버로 전송하는 중 에러 발생:', error);
+//     }
+//   } else {
+//     console.log('토큰을 가져오지 못했습니다.');
+//   }
+// }
 
-sendTokenToServer();
+// sendTokenToServer();
