94aace65ce0ed70c06d3e870eed412ed08ca9499
김지수 <spritaa@gmail.com>
2023-11-05 15:40:34 +0900

    REFACTOR<Server>: add enum type of push message, notification exception handling
    Former-commit-id: 9b2dee6e8372213db818b81d2a3d619de7c13a8f

--- BE/hanol/src/main/java/com/ssafy/hanol/notification/service/NotificationTokenService.java
+++ BE/hanol/src/main/java/com/ssafy/hanol/notification/service/NotificationTokenService.java
@@ -1,6 +1,8 @@
 package com.ssafy.hanol.notification.service;
 
+import com.ssafy.hanol.common.exception.CustomException;
 import com.ssafy.hanol.member.domain.Member;
+import com.ssafy.hanol.member.exception.MemberErrorCode;
 import com.ssafy.hanol.member.repository.MemberRepository;
 import com.ssafy.hanol.notification.controller.dto.request.FcmTokenApiRequest;
 import com.ssafy.hanol.notification.domain.NotificationToken;
@@ -25,19 +27,17 @@ public class NotificationTokenService {
     public void addToken(FcmTokenRequest fcmTokenRequest) {
         Long memberId = fcmTokenRequest.getMemberId();
         String token = fcmTokenRequest.getFcmToken();
-
-        Member member = memberRepository.findById(memberId).orElseThrow(); // TODO 예외처리
+        Member member = findMemberByMemberId(memberId);
 
         // fcm token 으로 NotificationToken 객체 조회. 존재하지 않으면 새로운 객체 생성.
         NotificationToken notificationToken = notificationTokenRepository.findByToken(token)
                 .orElseGet(NotificationToken::createEmpty);
         log.info("기존 notificationToken: {}", notificationToken);
         
-        // 기존 Notification 객체와 memberId가 다른 경우, 기존 객체를 삭제하고 새로운 객체 생성.
+        // 기존 Notification 객체와 memberId가 다른 경우, 기존 객체를 삭제하고 새로운 객체 생성. (같은 브라우저에서 새로운 회원이 로그인)
         if (isTokenHasDifferentMember(notificationToken, memberId)) {
             notificationTokenRepository.delete(notificationToken);
             notificationToken = NotificationToken.createEmpty();
-            log.info("같은 브라우저에서 새로운 회원이 로그인하여 기존 notificationToken 삭제 후 새로 생성함");
         }
 
         // 객체 최신 정보로 업데이트
@@ -47,13 +47,20 @@ public class NotificationTokenService {
     }
 
 
+    // FCM token 삭제
+    public void deleteToken(FcmTokenApiRequest fcmTokenApiRequest) {
+        notificationTokenRepository.deleteByToken(fcmTokenApiRequest.getFcmToken());
+    }
+
+    private Member findMemberByMemberId(Long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(() -> new CustomException(MemberErrorCode.NOT_FOUND_MEMBER));
+    }
+
     private boolean isTokenHasDifferentMember(NotificationToken notificationToken, Long memberId) {
         return notificationToken.getMember() != null && !notificationToken.getMember().getId().equals(memberId);
     }
 
 
-    public void deleteToken(FcmTokenApiRequest fcmTokenApiRequest) {
-        notificationTokenRepository.deleteByToken(fcmTokenApiRequest.getFcmToken());
-    }
+
 
 }
