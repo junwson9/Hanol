a2b31e4170fc594a6bd7847686717f0c99aa2487
nahye2772 <nahye2772@naver.com>
2023-11-06 00:26:14 +0900

    FEAT: update about-noti alarm feature
    Former-commit-id: 4a36015339b63a3372ef969b63209c5b62261395

--- FE/hanol/src/pages/about/aboutNotiSetting.tsx
+++ FE/hanol/src/pages/about/aboutNotiSetting.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-// import axiosInstance from 'api/axiosInterceptor';
-import axios from 'axios';
+import axiosInstance from 'api/axiosInterceptor';
+// import axios from 'axios';
 import NotiSettingComponent from 'components/About/NotiSettingComponent';
 import TapBarDepth2 from 'components/common/TapBarDepth2';
 import { aboutnotiSettingType } from 'types/DiagnosisResult';
@@ -16,16 +16,15 @@ const aboutNotiSetting = () => {
   const [notiSettingData, setNotiSettingData] = useState<aboutnotiSettingType>(initData);
   // const [checkRoutine, setCheckRoutine] = useState<boolean>(false);
   // const [individualRoutine, setIndividualRoutine] = useState<boolean>(false);
-
   useEffect(() => {
     // axiosInstance
     // .get('/diagnoses?limit=20')
-    // axiosInstance
-    axios
-      .get('http://localhost:4000/notifications')
+    axiosInstance
+      // axios
+      .get('/notifications')
       .then((response) => {
-        console.log('알림 설정 조회 성공:', response);
-        setNotiSettingData(response.data);
+        console.log('알림 설정 조회 성공:', response.data.data);
+        setNotiSettingData(response.data.data);
       })
       .catch((error) => {
         console.error('알림 설정 조회 실패:', error);
@@ -48,12 +47,18 @@ const aboutNotiSetting = () => {
         desc="저녁 9시 루틴을 수행했는지 확인해보세요."
         toggleState={notiSettingData.is_check_routine_active}
         noti_type="CHECK_ROUTINE"
+        onToggle={(newState) =>
+          setNotiSettingData((prevState) => ({ ...prevState, is_check_routine_active: newState }))
+        }
       />
       <NotiSettingComponent
         title="데일리 루틴 개별 알림"
         desc="루틴 별로 설정한 시간에 알림을 드려요."
         toggleState={notiSettingData.is_individual_routine_active}
         noti_type="INDIVIDUAL_ROUTINE"
+        onToggle={(newState) =>
+          setNotiSettingData((prevState) => ({ ...prevState, is_individual_routine_active: newState }))
+        }
       />
     </div>
   );
