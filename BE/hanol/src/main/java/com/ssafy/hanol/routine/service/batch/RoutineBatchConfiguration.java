c75c2539cd1250e9f866fd2fc5a09daef9b5019d
Jisoo <spritaa@gmail.com>
2023-11-06 18:01:07 +0900

    FIX: fix spring batch config
    Former-commit-id: 8f5f5bcfbffd243420caf755ee2eb742f66823c3

--- BE/hanol/src/main/java/com/ssafy/hanol/routine/service/batch/RoutineBatchConfiguration.java
+++ BE/hanol/src/main/java/com/ssafy/hanol/routine/service/batch/RoutineBatchConfiguration.java
@@ -1,6 +1,8 @@
 package com.ssafy.hanol.routine.service.batch;
 
 import com.ssafy.hanol.routine.domain.MemberRoutine;
+import com.ssafy.hanol.routine.domain.MemberRoutineLog;
+import com.ssafy.hanol.routine.repository.MemberRoutineLogRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -8,12 +10,16 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JpaPagingItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManagerFactory;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @Slf4j
@@ -56,19 +62,34 @@ public class RoutineBatchConfiguration {
         });
     }
 
-    /**
-     * JpsPagingItemReader: 
-     * @return
-     */
+
     @Bean
     public JpaPagingItemReader<MemberRoutine> memberRoutineJpaPagingItemReader() {
         JpaPagingItemReader<MemberRoutine> reader = new JpaPagingItemReader<>();
         reader.setEntityManagerFactory(entityManagerFactory);
-        reader.setPageSize(CHUNK_SIZE); // 한 번에 읽어올 데이터 수
+        reader.setPageSize(CHUNK_SIZE); // 한 번에 읽어올 데이터의 수
 
         reader.setQueryString("SELECT mr FROM MemberRoutine mr");
+        return reader;
+    }
 
+    @Bean
+    public ItemWriter<MemberRoutineLog> memberRoutineLogItemWriter(MemberRoutineLogRepository memberRoutineLogRepository) {
+        return items -> {
+            List<MemberRoutineLog> routineLogs = new ArrayList<>();
+            LocalDate today = LocalDate.now();
 
+            for (MemberRoutineLog memberRoutine : items) {
+                MemberRoutineLog routineLog = MemberRoutineLog.builder()
+                        .member(memberRoutine.getMember())
+                        .routine(memberRoutine.getRoutine())
+                        .date(today)
+                        .isDone(false)
+                        .build();
+                routineLogs.add(routineLog);
+            }
+            memberRoutineLogRepository.saveAll(routineLogs);
+        };
     }
 
 }
