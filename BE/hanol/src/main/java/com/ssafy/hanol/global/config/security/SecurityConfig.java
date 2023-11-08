eb8f1bff9c94439ede72d794b9196b29b1ba0607
김지수 <spritaa@gmail.com>
2023-11-08 17:09:10 +0900

    FEAT<Server>: implement sending diagnosis api
    Former-commit-id: 052fbe17504ef20db03903bc4200171ea0413e4b

--- BE/hanol/src/main/java/com/ssafy/hanol/global/config/security/SecurityConfig.java
+++ BE/hanol/src/main/java/com/ssafy/hanol/global/config/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,6 +13,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsUtils;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
