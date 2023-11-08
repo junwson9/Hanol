566ec4ca3c0e0b63d90084fff7ea2bf1e3ea1b4e
김지수 <spritaa@gmail.com>
2023-11-08 12:29:07 +0900

    FIX<Server>: change type of diagnosis' values from int to Integer
    Former-commit-id: 8c8657bb65ec3edac99fde781a0e07256f5c8d4b

--- BE/hanol/src/main/java/com/ssafy/hanol/diagnosis/repository/JpaDiagnosisRepository.java
+++ BE/hanol/src/main/java/com/ssafy/hanol/diagnosis/repository/JpaDiagnosisRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface JpaDiagnosisRepository extends JpaRepository<Diagnosis, Long> {
-    Optional<Diagnosis> findTopByMemberIdOrderByIdDesc(Long memberId);
+    Optional<Diagnosis> findTopByMemberIdAndValue1IsNotNullOrderByIdDesc(Long memberId);
 }
