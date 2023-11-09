3092f03c11df985d2b92feb5cb8fa2248acee446
Jisoo <spritaa@gmail.com>
2023-11-10 02:30:45 +0900

    FEAT: add batch test code
    Former-commit-id: c58af98368c2ed1c0b59dcbec22405d4c4b78947

---
+++ BE/hanol/src/test/java/com/ssafy/hanol/TestBatchConfig.java
@@ -0,0 +1,25 @@
+package com.ssafy.hanol;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 배치 테스트 환경을 설정하는 클래스
+ * 모든 테스트 클래스에 설정하는 불필요함을 없애기 위해 사용
+ */
+
+@Configuration
+@EnableAutoConfiguration
+@EnableBatchProcessing
+@ComponentScan(basePackages = "com.ssafy.hanol.routine")
+public class TestBatchConfig {
+
+    @Bean
+    public JobLauncherTestUtils jobLauncherTestUtils() {
+        return new JobLauncherTestUtils();
+    }
+}
