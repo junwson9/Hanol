0554d2fdde12115b8bdd8d06fb5e0a45a10cbf9b
HBumzz <choi12920@naver.com>
2023-11-10 16:49:50 +0900

    FIX : QA-fix, serviceworker-cache problem resolve
    Former-commit-id: 99480f4b07cb452987bdb7ee9211e8e93d4290bd

--- FE/hanol/src/pages/diagnose/diagnosisDetail.tsx
+++ FE/hanol/src/pages/diagnose/diagnosisDetail.tsx
@@ -11,22 +11,26 @@ import DivisionRectangle from 'components/common/DivisionRectangle';
 import RecommendCareRoutine2 from 'components/diagnosisResultPage/RecommendCareRoutine2';
 import TopBarDepth2 from 'components/common/TapBarDepth2';
 import { useRecoilValue } from 'recoil';
-import { diagnoseState } from 'recoil/atoms';
+import { diagnoseIdState, diagnoseState, PartState } from 'recoil/atoms';
 import { ImageState } from 'recoil/atoms';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import ResultSender from 'components/diagnosisResultPage/ResultSender';
 
 const DiagnosisDetail = () => {
   const navigate = useNavigate();
   const values = useRecoilValue(diagnoseState);
   const image = useRecoilValue(ImageState);
+  const scanPart = useRecoilValue(PartState);
+  const diagnoseId = useRecoilValue(diagnoseIdState);
   // viewBoolean 배열 초기화
+  console.log(diagnoseId);
   const initialViewBoolean = new Array(6).fill(false);
   const [viewBoolean, setViewBoolean] = useState(initialViewBoolean);
 
   // values 배열의 각 항목을 검사하여 viewBoolean 업데이트
   useEffect(() => {
-    const updatedViewBoolean = values.map((value) => value <= 1);
+    const updatedViewBoolean = values.map((value) => value >= 2);
     if (!updatedViewBoolean.includes(true)) {
       updatedViewBoolean.push(true);
     }
@@ -48,7 +52,7 @@ const DiagnosisDetail = () => {
       <div className="grid grid-cols-6 gap-[10px] mx-[23px]">
         <div className="col-span-full">
           <TopBarDepth2 name={'진단결과'} propsIsBack={false} rightBtnType={2} onClick={handleButtonClick} />
-          <AlopeciaDiagnosis valuenumber={Math.abs(values[5] - 3)} />
+          <AlopeciaDiagnosis valuenumber={values[5]} />
           <ScalpScaleView
             value1={values[0]}
             value2={values[1]}
@@ -58,10 +62,14 @@ const DiagnosisDetail = () => {
             value6={values[5]}
           />
           <DiagnosisDetailResult viewBoolean={viewBoolean} />
-          <ScalpImageView scalp_img={image} />
+          <ScalpImageView scalp_img={image} sub_title={scanPart} />
           <EmptyButton name="지난 내역 확인 하기" onClick={() => handleButtonClick()} />
+          <div>
+            <ResultSender diagnoseId={diagnoseId} />
+          </div>
         </div>
       </div>
+
       <div>
         <DivisionRectangle />
         <div className="grid grid-cols-6 gap-[10px] mx-[23px]">
