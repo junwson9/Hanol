797db2506418b8fe09b9efb71b85a725b4b50aec
최홍준 <choi12920@naver.com>
2023-11-10 17:01:28 +0900

    FIX<Client>: service worker precache fix
    Former-commit-id: 577517e52821a3f05b3574c8b8071decdaa40d21

--- FE/hanol/src/service-worker.ts
+++ FE/hanol/src/service-worker.ts
@@ -1,5 +1,4 @@
 /// <reference lib="webworker" />
-/* eslint-disable */
 /* eslint-disable no-restricted-globals */
 
 // This service worker can be customized!
@@ -11,7 +10,7 @@
 
 import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
-import { createHandlerBoundToURL, cleanupOutdatedCaches } from 'workbox-precaching';
+import { createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { StaleWhileRevalidate } from 'workbox-strategies';
 
@@ -25,8 +24,8 @@ clientsClaim();
 // even if you decide not to use precaching. See https://cra.link/PWA
 
 // 캐시 의심 부분
-const ignored = self.__WB_MANIFEST;
-cleanupOutdatedCaches();
+precacheAndRoute(self.__WB_MANIFEST);
+
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
 // https://developers.google.com/web/fundamentals/architecture/app-shell
