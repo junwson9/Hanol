5a60f806346a0c3e08c830dbf51c09e95db780fe
HBumzz <choi12920@naver.com>
2023-11-13 15:29:02 +0900

    Hotfix: add pwa download button
    Former-commit-id: e788c6e4de87bfcdf5398fd9704253fc1c7b104a

--- FE/hanol/src/components/common/useA2HS.js
+++ FE/hanol/src/components/common/useA2HS.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 const useA2HS = () => {
   const [deferredPrompt, setDeferredPrompt] = useState(null);
@@ -35,10 +36,44 @@ const A2HS = () => {
 
   return deferredPrompt ? (
     <div>
-      <button onClick={clearPrompt}>취소</button>
-      <button onClick={installApp}>홈 화면에 추가</button>
+      <SnackbarContainer>
+        <div className="button-container">
+          <button onClick={installApp}>앱으로 이용하기</button>
+          <button onClick={clearPrompt}>취소</button>
+        </div>
+      </SnackbarContainer>
     </div>
   ) : null;
 };
+
+const SnackbarContainer = styled.div`
+  position: fixed;
+  bottom: 6rem;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  align-items: center;
+  padding: 1rem 1rem;
+  background-color: #fff;
+  color: #fff;
+  border-radius: 12px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  max-width: calc(100% - 46px);
+  width: min(404px, calc(100% - 46px));
+  color: #3fcc8a;
+  border: 1px solid #3fcc8a;
+  z-index: 11111;
+
+  .button-container {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  button {
+    flex: 1;
+    margin: 0 8px; /* Adjust margin as needed */
+  }
+`;
 
 export default A2HS;
