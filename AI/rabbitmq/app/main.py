e56fb70ec26f29cc1f661373f3320efbde2f76ce
kimjunhyun <idkim8671@gmail.com>
2023-11-13 12:50:08 +0900

    FIX: AI-server Final Fix
    Former-commit-id: 22d060005091c02e14a00c77d371657f1d3987ad

--- AI/rabbitmq/app/main.py
+++ AI/rabbitmq/app/main.py
@@ -5,20 +5,23 @@ import requests
 from base64 import b64decode
 from dotenv import load_dotenv
 import os
+import json
+import base64
 
 app = FastAPI()
 load_dotenv()
+api_key = os.getenv("OPEN_API_KEY")
 
-host = os.getenv("host")
-port = os.getenv("port")
-username = os.getenv("username")
-password = os.getenv("password")
+headers = {
+         "Content-Type": "application/json",
+         "Authorization": f"Bearer {api_key}"
+        }
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(
-        host=host,
-        port=port,
-        credentials=pika.PlainCredentials(username=username, password=password))
+        host='15.165.74.87',
+        port=40000,
+        credentials=pika.PlainCredentials(username='admin', password='ssafya205!'))
     )
 channel = connection.channel()
 
@@ -36,37 +39,79 @@ exchange = 'scalp-diagnose-exchange'
 channel.queue_declare(queue=queue_response_name, durable=True)
 
 def request_handler(ch, method, properties, body):
-    print("되는중1")
     try:
         data = json.loads(body)
-        print(data.get("key_id"))
         key_id = data.get("key_id")
         sse_id = data.get("sse_id")
         image = data.get("image")
         image_bytes = b64decode(image)
-        print(sse_id)
-        request_url = "http://fastapi-ai:7000/image"
+        base64_image = base64.b64encode(image_bytes).decode('utf-8')
+        
+        payload = {
+                "model": "gpt-4-vision-preview",
+                "messages":[
+                    {
+                        "role": "user",
+                        "content": [
+                            {
+                                "type": "text",
+                                "text": "Is this image human scalp? "
+                                        +"If yes, answer must be start with yes. "
+                                        +"If no, answer must be start with no."
+                                },
+                            {
+                                "type": "image_url",
+                                "image_url":{
+                                    "url": f"data:image/jpg;base64,{base64_image}"
+                                    }
+                                }
+                            ]
+                        }
 
-        print(request_url)
-        print("서버 요청 보냄")
 
-        response = requests.post(request_url,
+                    ],
+                "max_tokens": 300
+                }
+        response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+        response_json = response.text
+        response_data = json.loads(response_json)
+        content = response_data['choices'][0]['message']['content']
+        print("gpt 응답: ",content)
+
+        if "Yes" in content:
+            flag = "Yes"
+        elif "yes" in content:
+            flag = "Yes"
+        else:
+            flag = "No"
+
+        if flag == "Yes":
+            request_url = "http://fastapi-ai:7000/image"
+            
+            print("서버 요청 보냄")
+            response = requests.post(request_url,
                     files={'image_bytes': image_bytes},
                     data={'sse_id': sse_id})
-
-
-        result = response.json()
-
-        sse_id = result.get("sse_id")
-        value1 = result.get("value1")
-        value2 = result.get("value2")
-        value3 = result.get("value3")
-        value4 = result.get("value4")
-        value5 = result.get("value5")
-        value6 = result.get("value6")
-        key_id = key_id
-
-        # print("서버 응답 돌아옴")
+            result = response.json()
+            sse_id = result.get("sse_id")
+            value1 = result.get("value1")
+            value2 = result.get("value2")
+            value3 = result.get("value3")
+            value4 = result.get("value4")
+            value5 = result.get("value5")
+            value6 = result.get("value6")
+            key_id = key_id
+        else:
+            key_id = key_id
+            sse_id = sse_id
+            value1 = -1
+            value2 = -1
+            value3 = -1
+            value4 = -1
+            value5 = -1
+            value6 = -1
+        print("서버 응답 돌아옴")
         response_data = {
                 "key_id": key_id,
                 "sse_id": sse_id,
@@ -78,22 +123,7 @@ def request_handler(ch, method, properties, body):
                 "value6": value6
         }
 
-        '''
-        response_data = {
-                "key_id": key_id,
-                "sse_id": sse_id,
-                "value1": 0,
-                "value2": 1,
-                "value3": 3,
-                "value4": 0,
-                "value5": 1,
-                "value6": 1
-        }
-        '''
-
-
         body = json.dumps(response_data).encode('utf-8')
-        print("여기까지도 됨")
 
         channel.basic_publish(exchange=exchange,
                             routing_key=response_routing_key,
@@ -112,4 +142,3 @@ async def startup_event():
     # RabbitMQ에서 메시지 처리 시작
     print("RabbitMQ 메시지 처리 시작")
     channel.start_consuming()
-    print("됨?")
